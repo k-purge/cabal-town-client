df69a52f6f1d431d5ca842cd0006d7a4e073e2ae
HIGGS <playerone@flareprotocol.xyz>
2024-08-30 00:48:21 +0800

    Add log

--- src/store/user-store/useUserStore.ts
+++ src/store/user-store/useUserStore.ts
@@ -62,6 +62,7 @@ function useUserStore() {
     if (window.Telegram.WebApp) {
       // Initialize the Web App
       window.Telegram.WebApp.ready();
+      console.log("window.Telegram.WebApp.initDataUnsafe: ", window.Telegram.WebApp.initDataUnsafe);
 
       // Get user information
       const user = window.Telegram.WebApp.initDataUnsafe.user;
