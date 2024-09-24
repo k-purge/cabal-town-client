03d5e251a0046b142426182ed762237541ecb1a8
chwchiu <chwchiu@connect.ust.hk>
2024-09-24 22:06:57 +0800

    Empty state for profile

---
+++ src/pages/profile/EmptyCard/index.tsx
@@ -0,0 +1,28 @@
+import { Box } from "@mui/material";
+import { CardText, CardContainer, CardContent, SubmitButton } from "./styles";
+import walletIcon from "assets/icons/wallet.svg";
+import { MobileMenu } from "components/header/headerMenu/HeaderMenu";
+import { useTonConnectModal } from "@tonconnect/ui-react";
+
+interface EmptyCardProps {
+  isLoading?: boolean;
+}
+
+export const EmptyCard = () => {
+  const { open } = useTonConnectModal();
+
+  return (
+    <CardContainer>
+      <CardContent>
+        <Box>
+          <CardText>Your Portfolio is Empty.</CardText>
+          <CardText>You should connect your wallet.</CardText>
+          <SubmitButton onClick={open}>
+            <img src={walletIcon} alt="wallet" />
+            Connect
+          </SubmitButton>
+        </Box>
+      </CardContent>
+    </CardContainer>
+  );
+};
